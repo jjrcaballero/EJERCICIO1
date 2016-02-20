a5c7b6fe6fa6c51cfaaab33002f9378f58ec0b2a
Jhon Jairo Roldan Caballero <jjrcaballero@gmail.com>
2016-02-20 18:58:47 -0500

    prueba

--- Lineas/Form1.Designer.cs
+++ Lineas/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(123, 119);
+            this.label1.Location = new System.Drawing.Point(55, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
